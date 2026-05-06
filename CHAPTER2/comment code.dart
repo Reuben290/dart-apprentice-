deb687a1b86781b6a40ba2d9127f4a4ea4047c68
Reuben290 <neequaye.dev@gmail.com>
2026-05-06 20:58:22 +0000

    progress

---
+++ CHAPTER2/comment code.dart
@@ -0,0 +1,1 @@
+// this is a comment i can use to create notes for myself and read them 
---
+++ CHAPTER2/statement and expressions.dart
@@ -0,0 +1,5 @@
+//this is a statement, a staement is a command,something you tell the computer to do
+void main(){
+  print("Hello, Dart Apprentice reader!");//a simple statement ends with a semicolon
+  
+}
