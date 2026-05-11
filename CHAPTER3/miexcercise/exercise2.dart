907cd606b19d33dc1f0883f0e2d598be490734c1
Reuben290 <neequaye.dev@gmail.com>
2026-05-11 20:47:54 +0000

    more progress

---
+++ CHAPTER3/miexcercise/exercise2.dart
@@ -0,0 +1,7 @@
+void main(){
+  const firsName = 'Reuben';
+  const lastName = 'Neequaye';
+  const fullName = '$firsName $lastName';
+  const myDetails = 'Hello, my name is $fullName';
+  print(myDetails);
+}
